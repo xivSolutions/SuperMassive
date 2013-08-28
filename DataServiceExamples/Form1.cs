cd73509d483cb9d4160f0f53ad2fc9a103969a2c
xivSolutions <xivSolutions@gmail.com>
2013-08-27 21:47:11 -0500

    Added Data service test project

--- DataServiceExamples/Form1.cs
+++ DataServiceExamples/Form1.cs
@@ -7,6 +7,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using ChinnookDb;
+using SuperMassive;
 
 
 namespace DataServiceExamples
@@ -20,7 +22,26 @@ namespace DataServiceExamples
 
         private void Form1_Load(object sender, EventArgs e)
         {
+            var newCustomer = new Customer { FirstName = "John", LastName = "Atten", Company = "xivSolutions", Email = "xivSolutions@gmail.com" };
+            this.insertCustomer(newCustomer);
+            var customers = this.getCustomers();
+            foreach (var customer in customers)
+            {
+                Console.WriteLine("{0} {1} ID: {2}", customer.FirstName, customer.LastName, customer.CustomerId);
+            }
+        }
 
+        IEnumerable<Customer> getCustomers()
+        {
+            var tbl = new DynamicModel("ChinookDb", "Customer", "CustomerId");
+            return tbl.All<Customer>();
+        }
+
+
+        void insertCustomer(Customer customer)
+        {
+            var tbl = new DynamicModel("ChinookDb", "Customer", "CustomerId");
+            tbl.Insert(customer);
         }
     }
 }
